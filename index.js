f930b41323185cb8fd723d51c9d027d73c654716
Johan Gavelan <johangavelan@gmail.com>
2020-08-10 19:51:24 -0500

    same x same o

--- index.js
+++ index.js
@@ -166,3 +166,35 @@ function squareDigits(num) {
 }
 
 squareDigits(223);
+
+
+//SAME X SAME O 
+function XO(str) {
+  let xCount = 0;
+  let oCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() === 'x' ) {
+      xCount++;
+    }
+
+    if (str[i].toLowerCase() === 'o' ) {
+      oCount++;
+    }  
+  }
+
+  if (xCount === 0 && oCount === 0) {
+    return true;
+  }
+
+  else if (xCount === oCount) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+}
+
+
+XO('ooxx')
