a5c9239231bb33c53f19b92ff13019fbda8e3b5b
Johan Gavelan <johangavelan@gmail.com>
2020-08-12 19:50:40 -0500

    accessing multiple elements

--- accessing-dom-elements/app.js
+++ accessing-dom-elements/app.js
@@ -5,3 +5,20 @@ let logoEl = document.getElementById("logo");
 console.log(logoEl);
 
 console.log(document.querySelector(".chore"));
+
+
+//Accessing multiple elements
+//By Class
+let choresEl = document.getElementsByClassName("chore");
+console.log(choresEl);
+
+//By Tags
+let choresOpt = document.getElementsByTagName("li");
+console.log(choresOpt);
+
+let headingEl = document.getElementsByTagName("h2");
+console.log(headingEl);
+
+//All
+let paragraphEl = document.querySelectorAll(".chore, .random-paragraphs");
+console.log(paragraphEl);
