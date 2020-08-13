3d176d3dd492fa9746481e7c80f8df25f3d6544a
Johan Gavelan <johangavelan@gmail.com>
2020-08-12 21:15:32 -0500

    Local example.com stylized from Javascript

---
+++ changing-css/app.js
@@ -0,0 +1,20 @@
+const bodySt = document.querySelector("body").style;
+const divSt = document.querySelector("div").style;
+const aSt = document.querySelector("a:link, a:visited").style;
+
+bodySt.backgroundColor = "#f0f0f2";
+bodySt.margin = 0;
+bodySt.padding = 0;
+bodySt.fontFamily = "sans-serif";
+
+divSt.width = "600px";
+divSt.margin = "5em auto";
+divSt.padding = "2em";
+divSt.backgroundColor = "#fdfdff";
+divSt.borderRadius = "0.5em";
+divSt.boxShadow = "2px 3px 7px 2px rgba(0,0,0,0.02)";
+
+aSt.color = "#38488f";
+aSt.textDecoration = "none";
+
+
