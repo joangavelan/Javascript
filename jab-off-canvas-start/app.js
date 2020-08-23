e0f9be0fbbd006161db87f32192a56786374dc1f
Johan Gavelan <johangavelan@gmail.com>
2020-08-22 23:47:42 -0500

    elements saved in variables

---
+++ jab-off-canvas-start/app.js
@@ -0,0 +1,4 @@
+const navTriggerEl = document.querySelector(".hamburger");
+const navEl = document.querySelector("nav");
+const contentEl = document.querySelector(".content");
+const hamburgerBarsEl = document.getElementsByTagName("span");
