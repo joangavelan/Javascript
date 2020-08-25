2ccfa195fd361a40bef04b6e31166ee6bb7e03ce
Johan Gavelan <johangavelan@gmail.com>
2020-08-24 21:18:05 -0500

    toggle functionality in hamburger icon, content and nav set

--- jab-off-canvas-start/app.js
+++ jab-off-canvas-start/app.js
@@ -2,3 +2,18 @@ const navTriggerEl = document.querySelector(".hamburger");
 const navEl = document.querySelector("nav");
 const contentEl = document.querySelector(".content");
 const hamburgerBarsEl = document.getElementsByTagName("span");
+
+
+function click() {
+    navEl.classList.toggle("open");
+    contentEl.classList.toggle("shift");
+    
+    for (let item of hamburgerBarsEl) {
+        item.classList.toggle("change");
+    }
+}
+
+function toggleNav() {
+    navTriggerEl.addEventListener("click", click)
+}
+toggleNav();
