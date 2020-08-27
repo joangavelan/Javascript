09453787e824a62e82d0b62f508c766714c9f9e5
Johan Gavelan <johangavelan@gmail.com>
2020-08-26 22:48:57 -0500

    images replaced, variables created in js

---
+++ roll-dice/app.js
@@ -0,0 +1,5 @@
+const diceTriggerEl = document.querySelector(".dice__trigger");
+const diceImageEl = document.querySelector(".dice__image");
+
+console.log(diceTriggerEl);
+console.log(diceImageEl);
