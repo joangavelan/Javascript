71ead6b6d5eee973e47cdbf53525f5a41ef06512
Johan Gavelan <johangavelan@gmail.com>
2020-08-28 19:31:29 -0500

    Challenge completed, dice rolling up animation

--- roll-dice/app.js
+++ roll-dice/app.js
@@ -1,14 +1,22 @@
 const diceTriggerEl = document.querySelector(".dice__trigger");
 const diceImageEl = document.querySelector(".dice__image");
+const animationEl = document.querySelector(".animated");
 
 function getRandomNumber() {
     return 1 + Math.floor(Math.random() * 6);
 }
 
+
 function showDice() {
     diceTriggerEl.addEventListener("click", function() {
         let myRandomNumber = getRandomNumber();
         diceImageEl.src = "images/dice" + myRandomNumber + ".png";
+        diceImageEl.classList.add("animated");
+        
+        setTimeout(function () {
+            diceImageEl.classList.remove("animated");
+        }, 700);
+
     })
 }
 
