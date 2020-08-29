ef46d2d900f09509e00c0ff5645ff02e87468e74
Johan Gavelan <johangavelan@gmail.com>
2020-08-29 00:11:01 -0500

    store item function name changed

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -6,7 +6,7 @@ const todoItemEls = document.querySelectorAll(".todo__item");
 function addListItem() {
         todoInputEl.addEventListener("keypress", function(event) {
             if(event.keyCode === 13) {
-                let task = storageItem(todoInputEl.value);
+                let task = storeItem(todoInputEl.value);
                 todoListlEl.appendChild(task);
                 todoInputEl.value = '';
            }  
@@ -14,7 +14,7 @@ function addListItem() {
      
 }
 
-function storageItem(value) {
+function storeItem(value) {
     let newListItem = document.createElement("li");
     newListItem.textContent = value;
     newListItem.classList.add("todo__item");
