faae9fd76861075a7d476464d547afcb7cbfd296
Johan Gavelan <johangavelan@gmail.com>
2020-09-04 20:10:59 -0500

    todo list app done

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -6,15 +6,33 @@ const todoItemEls = document.querySelectorAll(".todo__item");
 function addListItem() {
         todoInputEl.addEventListener("keypress", function(event) {
             if(event.keyCode === 13) {
-                let task = storeItem(todoInputEl.value);
-                todoListlEl.appendChild(task);
+                let task = createListItem(todoInputEl.value);
+                // todoListlEl.appendChild(task);
+                todoListlEl.insertBefore(task, todoListlEl.childNodes[0]);
                 todoInputEl.value = '';
            }  
-        })
-     
+        })    
 }
 
-function storeItem(value) {
+// This function doesn't work because of bubbling 
+// function toggleDone() {
+//     for (let elem of todoItemEls) {
+//         elem.addEventListener("click", function() {
+//             elem.classList.toggle("done");
+//         })
+//     }
+// }
+
+
+function toggleDone() {
+    todoListlEl.addEventListener("click", function(event) {
+        if (event.target.classList.contains("todo__item")) {
+            event.target.classList.toggle("done");
+        };
+    })
+}
+
+function createListItem(value) {
     let newListItem = document.createElement("li");
     newListItem.textContent = value;
     newListItem.classList.add("todo__item");
@@ -23,4 +41,5 @@ function storeItem(value) {
 
 
 addListItem();
+toggleDone();
 
