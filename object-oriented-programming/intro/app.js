32ce4ad87dbe6b82124e20800ba865320400bf0a
Johan Gavelan <johangavelan@gmail.com>
2020-10-03 08:30:44 -0500

    intro to object oriented programming, function constructor, and prototype

---
+++ object-oriented-programming/intro/app.js
@@ -0,0 +1,27 @@
+//function constructor 
+function Car(brand, model) {
+    this.brand = brand;
+    this.model = model;
+}
+
+Car.prototype.stats = function() {
+    return `${this.brand} ${this.model}!`;
+}
+
+Car.prototype.drive = function() {
+    return `I'm driving my new ${this.stats()}!`;
+}
+
+
+let car1 = new Car('Lambo', 'Aventador'); //instance
+let car2 = new Car('Toyota', 'Camry');
+let car3 = new Car('Nissan', 'Sentra');
+
+console.log(car3.drive());
+
+console.log(car3);
+
+// console.log(car1.stats());
+// console.log(car2.stats());
+// console.log(car3.stats());   
+
