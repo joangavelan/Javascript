d319040a402c415b448b467c384f359a286631b9
Johan Gavelan <johangavelan@gmail.com>
2020-10-26 11:53:24 -0500

    question function constructor

--- projects/quiz-application/js/app.js
+++ projects/quiz-application/js/app.js
@@ -1,26 +1,4 @@
-import Question from "./question.js";
-import Quiz from "./quiz.js";
+import Question from './question.js';
 
-const q1 = new Question(
-    "What's 2 + 2",
-    [2, 3, 4, 5],
-    2
+const q1 = new Question("What's 2 + 2?", [4, 2, 7, 3], 0);
 
-);
-const q2 = new Question(
-    "First president of US?",
-    ["AL", "George", "Barack", "Johnny"],
-    1
-);
-
-const qArray = [q1, q2];
-
-
-const myQuiz = new Quiz(qArray);
-
-console.log(myQuiz.questions)
-console.log(myQuiz.getCurrentQuestion())
-
-//button 
-myQuiz.nextIndex();
-console.log(myQuiz.getCurrentQuestion())
--- projects/quiz-application/js/question.js
+++ projects/quiz-application/js/question.js
@@ -1,9 +1,9 @@
 export default function Question(question, choices, answerKey) {
-    this.question = question;
-    this.choices = choices;
-    this.answerKey = answerKey;
+    this.question = question,
+    this.choices = choices,
+    this.answerKey = answerKey
 }
 
-Question.prototype.isCorrect = function (guessKey) {
+Question.prototype.isCorrect = function(guessKey) {
     return guessKey === this.answerKey;
 }
